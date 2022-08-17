fd107782724f066d064043311765cbe10e3c8764
Bartosz Sadowski <bshadowsky@gmail.com>
2022-08-17 15:19:18 +0200

    ApiResponse

--- DeskBookingSystem/DeskBookingSystem/Controllers/LocationController.cs
+++ DeskBookingSystem/DeskBookingSystem/Controllers/LocationController.cs
@@ -6,6 +6,7 @@ using DeskBookingSystem.Repository.IRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace DeskBookingSystem.Controllers
 {
@@ -14,75 +15,130 @@ namespace DeskBookingSystem.Controllers
 
     public class LocationController : ControllerBase
     {
+        protected APIResponse _response;
         private readonly ILocationRepository _locationRepo;
         private readonly IMapper _mapper;
         public LocationController(ILocationRepository locationRepo, IMapper mapper)
         {
             _locationRepo = locationRepo;
             _mapper = mapper;
+            this._response = new ();
         }
         [HttpGet]
-        public async Task<ActionResult <IEnumerable<LocationDTO>>> GetAllLocations()
+        public async Task<ActionResult <APIResponse>> GetAllLocations()
         {
-            IEnumerable<Location> locations = await _locationRepo.GetAll();
-            return Ok(_mapper.Map<List<LocationDTO>>(locations));
+            try
+            {
+                IEnumerable<Location> locations = await _locationRepo.GetAll();
+                _response.Result = _mapper.Map<List<LocationDTO>>(locations);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSucces=false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpGet("{id:int}",Name ="GetLocation")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<LocationDTO>> GetLocation(int id)
+        public async Task<ActionResult<APIResponse>> GetLocation(int id)
         {
-            if (id==0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var location = await _locationRepo.Get(x => x.Id == id);
+                if (location == null)
+                {
+                    return NotFound();
+                }
+                _response.Result = _mapper.Map<LocationDTO>(location);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
             }
-            var location = await _locationRepo.Get(x => x.Id == id);
-            if (location == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSucces = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
             }
-            return Ok(_mapper.Map<LocationDTO>(location));
+            return _response;
+
         }
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
        
-        public async Task<ActionResult<LocationCreateDTO>> CreateLocation([FromBody] LocationCreateDTO locationCreateDTO)
+        public async Task<ActionResult<APIResponse>> CreateLocation([FromBody] LocationCreateDTO locationCreateDTO)
         {
-            if (await _locationRepo.Get(u=>u.LocationName.ToLower()== locationCreateDTO.LocationName.ToLower())!= null)
+            try
             {
-                ModelState.AddModelError("", "Location already exists!");
-                return BadRequest(ModelState);
-            }
-            if (locationCreateDTO == null)
-            {
-                return BadRequest(locationCreateDTO);
-            }
-            Location model = _mapper.Map<Location>(locationCreateDTO);
 
-            await _locationRepo.Create(model);
-            return CreatedAtRoute("GetLocation",new { id = model.Id },model);
+
+                if (await _locationRepo.Get(u => u.LocationName.ToLower() == locationCreateDTO.LocationName.ToLower()) != null)
+                {
+                    ModelState.AddModelError("", "Location already exists!");
+                    return BadRequest(ModelState);
+                }
+                if (locationCreateDTO == null)
+                {
+                    return BadRequest(locationCreateDTO);
+                }
+                Location model = _mapper.Map<Location>(locationCreateDTO);
+
+                await _locationRepo.Create(model);
+                _response.Result = _mapper.Map<LocationDTO>(model);
+                _response.StatusCode = HttpStatusCode.Created;
+
+                return CreatedAtRoute("GetLocation", new { id = model.Id }, model);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpDelete("{id:int}",Name ="DeleteLocation")]
         [ProducesResponseType(204)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
-        public async Task<IActionResult> DeleteLocation(int id)
+        public async Task<ActionResult<APIResponse>> DeleteLocation(int id)
         {
-            if (id==0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var location = await _locationRepo.Get(u => u.Id == id);
+                if (location == null)
+                {
+                    return NotFound();
+                }
+                await _locationRepo.Remove(location);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSucces = true;
+                return Ok(_response);
             }
-            var location = await _locationRepo.Get(u=>u.Id==id);
-            if (location == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSucces = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
             }
-            await _locationRepo.Remove(location);
-            return NoContent();
+            return _response;
         }
     }
 }
---
+++ DeskBookingSystem/DeskBookingSystem/Models/APIResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace DeskBookingSystem.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSucces { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+
+        public object Result { get; set; }
+    }
+}
